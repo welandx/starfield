b1d0cfebd3c847d616ca6fa64f698243b5a2dbb2
weland <weland@bupt.edu.cn>
2024-02-17 21:23:50 +0800

    split file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use clap::Parser;
-use pinyin::ToPinyin;
-//use std::{ascii::AsciiExt, collections::HashMap, fmt::Debug};
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+mod word;
 
 #[derive(Parser)]
 struct Cli {
@@ -13,104 +11,6 @@ struct Cli {
     add: std::path::PathBuf,
 }
 
-fn word2(
-    hans: &String,
-    map: &HashMap<String, Vec<String>>,
-    danzi: &HashMap<String, Vec<String>>,
-    word: &HashMap<String, bool>,
-    code: &HashMap<String, bool>,
-) {
-    let han = &hans[..];
-    // convert to pinyin
-    let mut res = String::new();
-    let mut has_fly = false;
-    let mut res_fly = String::new();
-    if word.contains_key(hans) {
-        return;
-    }
-    for pinyin in han.to_pinyin() {
-        if let Some(pinyin) = pinyin {
-            let fly = map.get(pinyin.plain()).unwrap().len();
-            res.push_str(&map.get(pinyin.plain()).unwrap()[0]);
-            if fly == 2 {
-                has_fly = true;
-                res_fly.push_str(&map.get(pinyin.plain()).unwrap()[1])
-            } else {
-                res_fly.push_str(&map.get(pinyin.plain()).unwrap()[0]);
-            }
-        }
-    }
-    let mut index = 0;
-    if has_fly {
-        let mut index = 0;
-        while res_fly.len() < 6 {
-            if code.contains_key(&res_fly) {
-                res_fly.push(
-                    danzi.get(&hans[index..index + 3]).unwrap()[0]
-                        .chars()
-                        .nth(2)
-                        .unwrap(),
-                );
-                index += 3;
-            } else {
-                break;
-            }
-        }
-        println!("{}\t{}", han, res_fly);
-    }
-    while res.len() < 6 {
-        if code.contains_key(&res) {
-            res.push(
-                danzi.get(&hans[index..index + 3]).unwrap()[0]
-                    .chars()
-                    .nth(2)
-                    .unwrap(),
-            );
-            index += 3;
-        } else {
-            println!("{}\t{}", han, res);
-            return;
-        }
-    }
-    println!("{}\t{}", han, res);
-}
-
-fn word3(
-    hans: &String,
-    map: &HashMap<String, Vec<String>>,
-    danzi: &HashMap<String, Vec<String>>,
-    word: &HashMap<String, bool>,
-    code: &HashMap<String, bool>,
-) {
-    let han = &hans[..];
-    let mut res = String::new();
-    if word.contains_key(hans) {
-        return;
-    }
-    for pinyin in han.to_pinyin() {
-        if let Some(pinyin) = pinyin {
-            let sf = &map.get(pinyin.plain()).unwrap()[0];
-            let nth0 = sf.chars().nth(0).unwrap();
-            res.push(nth0);
-        }
-    }
-    let mut index = 0;
-    while res.len() < 6 {
-        if code.contains_key(&res) {
-            res.push(
-                danzi.get(&hans[index..index + 3]).unwrap()[0]
-                    .chars()
-                    .nth(2)
-                    .unwrap(),
-            );
-            index += 3;
-        } else {
-            println!("{}\t{}", han, res);
-            return;
-        }
-    }
-    println!("{}\t{}", han, res);
-}
 fn main() {
     let args = Cli::parse();
     //let sf_map: HashMap<String, String> = HashMap::new();
@@ -124,8 +24,8 @@ fn main() {
         let i = line.unwrap().to_string();
         let len = &i.len();
         match len / 3 {
-            2 => word2(&i, &map, &danzi, &word, &code),
-            3 | 4 => word3(&i, &map, &danzi, &word, &code),
+            2 => word::word2(&i, &map, &danzi, &word, &code),
+            3 | 4 => word::word3(&i, &map, &danzi, &word, &code),
             _ => {}
         };
     }
